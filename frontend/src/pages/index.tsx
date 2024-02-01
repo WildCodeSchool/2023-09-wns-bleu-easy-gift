1e2fcba18ada2cf6dc32c9da47021f49e3d09b4d
caudrel <lozachaurelie@gmail.com>
2024-02-01 10:34:46 +0100

    Apollo not starting problem 4000

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-       <p>Hello world !</p>
+        <p>Hello world !</p>
       </main>
     </>
   );
