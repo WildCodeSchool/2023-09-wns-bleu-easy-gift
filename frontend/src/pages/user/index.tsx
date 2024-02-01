e31d3303b98c014fb70c02f7358cccea0d40c13c
caudrel <lozachaurelie@gmail.com>
2024-02-01 11:55:51 +0100

    service unhealthy

--- frontend/src/pages/user/index.tsx
+++ frontend/src/pages/user/index.tsx
@@ -1,35 +1,20 @@
-import React from 'react';
-import { useUsersQuery } from '@/graphql/generated/schema';
+import React from "react";
+import { useUsersQuery } from "@/graphql/generated/schema";
 
-const { loading, data, error } = useUsersQuery();
+export default function Users() {
+  const { loading, data, error } = useUsersQuery();
 
-// export type RecentAd = {
-//   id: number;
-//   title: string;
-//   price: number;
-//   picture: string;
-// };
+  console.log(data);
 
-console.log(data);
-
-<section className="">
-        {users.map((user) => (
-          <AdCard key={user.id} user={user} link={/users/${user.id}} />
-        ))}
-      </section>
-
-export default function RecentAds() {
-  
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error : {error.message}</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
   const users = data?.users || [];
 
   return (
     <div className="">
       <h2 className="">Liste des utilisateurs</h2>
-
-
-      
+      {/* Render your users here */}
     </div>
   );
 }
