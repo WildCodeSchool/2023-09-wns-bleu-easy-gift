1fee8a2c90705ddcf4941b7da0eb1f5c1da47af7
Morgane Lejeune <morgane.lejeune@hotmail.fr>
2024-02-08 16:57:51 +0100

    first form login with logUser.gql

---
+++ frontend/src/pages/auth/index.tsx
@@ -0,0 +1,39 @@
+//frontend/src/pages/auth/login.tsx
+import { InputLogin, useLoginLazyQuery } from "@/graphql/generated/schema";
+
+function Login() {
+
+  const [login, {data, error}] = useLoginLazyQuery()
+  console.log(data, error );
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const data = Object.fromEntries(formData) as InputLogin;
+    if (data.email && data.password) {
+      login({
+        variables: { infos: { email: data.email, password: data.password } },
+      });
+    }
+  };
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24`}
+    >
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input type="text" name="email" placeholder="Indiquez votre email" />
+        </div>
+        <div>
+          <input
+            type="password"
+            name="password"
+            placeholder="Indiquez votre mot de passe"
+          />
+        </div>
+        <input type="submit" />
+      </form>
+    </main>
+  );
+}
+
+export default Login;
--- frontend/src/pages/user/index.tsx
+++ frontend/src/pages/user/index.tsx
@@ -6,7 +6,6 @@ export default function UserList() {
   const { loading, data, error } = useUsersQuery();
 
   const users = data?.users || [];
-  console.log("Notre data",data);
   return <ShowUsersList users={users} />;
 
 }
