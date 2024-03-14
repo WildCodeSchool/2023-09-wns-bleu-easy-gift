2c624c988215ca42f0b827a27b6c00b256de91f0
Olga Kuzkina <o.kuzkina@gmail.com>
2024-03-14 15:40:59 +0100

    fixed created entity group

--- backend/src/entities/group.ts
+++ backend/src/entities/group.ts
@@ -6,21 +6,32 @@ import {
     PrimaryGeneratedColumn,
 } from 'typeorm'
 import { Avatar } from './avatar'
+import { ObjectType, Field, Int } from 'type-graphql'
+import { Length } from 'class-validator'
 
+@ObjectType()
 @Entity()
 export class Group extends BaseEntity {
+    @Field(() => Int)
     @PrimaryGeneratedColumn()
     id: number
 
+    @Field()
     @Column()
+    @Length(3, 30, {
+        message: "Le nom de l'avatar doit contenir entre 3 and 30 caractères",
+    })
     name: string
 
+    @Field()
     @Column()
     created_at: Date
 
+    @Field()
     @Column()
-    modifited_at: Date
+    modified_at: Date
 
+    @Field(() => Avatar)
     @ManyToOne(() => Avatar, avatar => avatar.group)
     avatar: Avatar
 }
