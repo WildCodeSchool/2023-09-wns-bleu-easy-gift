b39ba068b166685acccea313dcea916b6a145d14
Olga Kuzkina <o.kuzkina@gmail.com>
2024-04-16 14:16:02 +0200

    added layout and variables stiles

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from "react";
-import { Button } from "../ui/button";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import Authentication from "../Authentication";
-import Cookies from "js-cookie";
-import Logout from "../Logout";
-import { checkUserConnected } from "@/utils/checkConnection";
-import { maxWidthScreen } from "@/constants/styles";
+import React, { useEffect, useState } from 'react';
+import { Button } from '../ui/button';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import Authentication from '../Authentication';
+import Cookies from 'js-cookie';
+import Logout from '../Logout';
+import { checkUserConnected } from '@/utils/checkConnection';
+import { maxWidthScreen } from '@/constants/styles';
 
 type TopState = {
   email: string;
@@ -23,22 +23,24 @@ export default function Navbar() {
 
   const pathname = usePathname();
 
-  const isLoginPage = pathname.includes("/auth");
+  const isLoginPage = pathname.includes('/auth');
 
   const isConnected = checkUserConnected();
 
   return (
-    <nav className="w-full h-16 sticky top-0 shadow-lg z-10 bg-bgPrimary">
+    <nav className='w-full h-16 sticky top-0 shadow-lg z-10 bg-bgPrimary'>
       <div
         className={`max-w-[${maxWidthScreen}] h-full flex justify-between items-center gap-2 mx-auto px-10`}
       >
-        <Link href="/">
-          <h1 className="font-rubik">Crazy Gift</h1>
+        <Link href='/'>
+          <h1 style={{ color: 'var(--primary-color)' }} className='font-rubik'>
+            Crazy Gift
+          </h1>
         </Link>
-        <div className="flex gap-2">
+        <div className='flex gap-2'>
           {!isConnected && (
-            <Button className="shadow-md">
-              <Link href="/auth/login">Connexion</Link>
+            <Button variant='default' className='shadow-md'>
+              <Link href='/auth/login'>Connexion</Link>
             </Button>
           )}
           <Logout />
