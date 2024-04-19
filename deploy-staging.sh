49285afb57293778ac2c9d745688f08287c76110
dlopoelinreverse <l.lesaul@gmail.com>
2024-04-19 12:18:52 +0200

    Create proper docker-compose.staging.yml

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 git fetch origin && git reset --hard origin/dev && git clean -f -d && \
-docker compose -f docker-compose.production.yml down && \
-docker compose -f docker-compose.production.yml pull && \
-docker compose -f docker-compose.production.yml --env-file .env.staging up -d;
+docker compose -f docker-compose.staging.yml down && \
+docker compose -f docker-compose.staging.yml pull && \
+docker compose -f docker-compose.staging.yml --env-file .env.staging up -d;
