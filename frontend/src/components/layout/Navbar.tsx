2b4b61f04d1c0eeaf3ffa215c6faf201df0a163a
Jeremie Pourageaux <jpourageaux@hotmail.fr>
2024-04-19 14:38:44 +0200

    chmod deploy-staging

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -1,9 +1,9 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { Button } from '../ui/button';
-import Link from 'next/link';
-import Logout from '../Logout';
-import { checkUserConnected } from '@/utils/checkConnection';
-import { maxWidthScreen } from '@/constants/styles';
+import React, { useState, useRef, useEffect } from "react";
+import { Button } from "../ui/button";
+import Link from "next/link";
+import Logout from "../Logout";
+import { checkUserConnected } from "@/utils/checkConnection";
+import { maxWidthScreen } from "@/constants/styles";
 
 export default function Navbar() {
   const [isMenuOpen, setMenuOpen] = useState(false);
@@ -18,87 +18,87 @@ export default function Navbar() {
         closeMenu();
       }
     };
-    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener("mousedown", handleClickOutside);
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
 
   return (
-    <nav className='w-full h-16 sticky top-0 shadow-lg z-10 bg-bgPrimary'>
+    <nav className="w-full h-16 sticky top-0 shadow-lg z-10 bg-bgPrimary">
       <div
         className={`max-w-[${maxWidthScreen}] h-full flex justify-between items-center gap-2 mx-auto px-10`}
       >
         {/* Burger Icon for Mobile Screens */}
-        <button onClick={() => setMenuOpen(!isMenuOpen)} className='md:hidden'>
+        <button onClick={() => setMenuOpen(!isMenuOpen)} className="md:hidden">
           <svg
-            className='h-6 w-6'
-            fill='none'
-            viewBox='0 0 24 24'
-            stroke='currentColor'
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
           >
             <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
+              strokeLinecap="round"
+              strokeLinejoin="round"
               strokeWidth={2}
-              d='M4 6h16M4 12h16M4 18h16'
+              d="M4 6h16M4 12h16M4 18h16"
             />
           </svg>
         </button>
 
         {/* Logo Link */}
-        <Link href='/'>
-          <h1 className='font-rubik'>Easy Gift</h1>
+        <Link href="/">
+          <h1 className="font-rubik">Easy Gift</h1>
         </Link>
 
         {/* Links for Larger Screens */}
         <div className={`hidden md:flex md:flex-row gap-4`}>
-          <Link href='#'>Mes groupes</Link>
-          <Link href='#'>Mes chats</Link>
-          <Link href='#'>Link 3</Link>
+          <Link href="#">Mes groupes</Link>
+          <Link href="#">Mes chats</Link>
+          <Link href="#">TestChmod</Link>
         </div>
 
         {/* Side Drawer for Mobile Screens */}
         <div
           ref={menuRef}
           className={`absolute left-0 top-0 w-[75%] h-110 bg-bgPrimary shadow-lg transform ${
-            isMenuOpen ? 'translate-x-0' : '-translate-x-full'
+            isMenuOpen ? "translate-x-0" : "-translate-x-full"
           } transition-transform duration-300 ease-in-out flex flex-col gap-4 p-4 md:hidden`}
         >
           {/* Close Button */}
-          <button onClick={() => setMenuOpen(false)} className='self-end'>
+          <button onClick={() => setMenuOpen(false)} className="self-end">
             <svg
-              className='h-6 w-6'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke='currentColor'
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
               <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 strokeWidth={2}
-                d='M6 18L18 6M6 6l12 12'
+                d="M6 18L18 6M6 6l12 12"
               />
             </svg>
           </button>
-          <Link href='#' onClick={closeMenu}>
+          <Link href="#" onClick={closeMenu}>
             Mes groupes
           </Link>
-          <Link href='#' onClick={closeMenu}>
+          <Link href="#" onClick={closeMenu}>
             Mes chats
           </Link>
-          <Link href='#' onClick={closeMenu}>
+          <Link href="#" onClick={closeMenu}>
             Link 3
           </Link>
           {!isConnected && (
             <>
-              <Button className='shadow-md'>
-                <Link onClick={closeMenu} href='/auth/login'>
+              <Button className="shadow-md">
+                <Link onClick={closeMenu} href="/auth/login">
                   Login
                 </Link>
               </Button>
-              <Button className='shadow-md'>
-                <Link onClick={closeMenu} href='/auth/login'>
+              <Button className="shadow-md">
+                <Link onClick={closeMenu} href="/auth/login">
                   Sign up
                 </Link>
               </Button>
@@ -111,11 +111,11 @@ export default function Navbar() {
         <div className={`hidden md:flex md:flex-row gap-2`}>
           {!isConnected && (
             <>
-              <Button className='shadow-md'>
-                <Link href='/auth/login'>Login</Link>
+              <Button className="shadow-md">
+                <Link href="/auth/login">Login</Link>
               </Button>
-              <Button className='shadow-md'>
-                <Link href='/auth/login'>Sign up</Link>
+              <Button className="shadow-md">
+                <Link href="/auth/login">Sign up</Link>
               </Button>
             </>
           )}
