aa8842a5b55a52f8c96a76541e24f4ff49e4514b
Morgane Lejeune <morgane.lejeune@hotmail.fr>
2024-06-12 17:06:40 +0200

    changes sections on tablette et desktop devices

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -26,8 +26,8 @@ export default function Home() {
   };
   return (
     <>
-      <section className='mb-16 min-h-40 h-auto flex flex-initial flex-wrap content-start items-center my-0 mx-auto w-4/5 md:mb-28 md:min-h-130 md:justify-evenly md:max-w-2xl lg:mb-40 lg:max-w-4xl xl:min-h-0 xl:mb-20 xl:max-w-7xl 2xl:max-w-[1800px] 2xl:content-center 2xl:mb-40 2xl:min-h-130'>
-        <div className='hidden relative w-full order-2 md:block  md:max-w-4xl md:min-w-96 lg:mb-24 lg:order-1 lg:max-w-xl:min-h-130 xl:max-w-lg xl:mb-0 2xl:max-w-3xl'>
+      <section className='mb-16 min-h-40 h-auto flex flex-initial flex-wrap content-start items-center my-0 mx-auto w-4/5 md:mb-28 md:min-h-80 md:justify-evenly md:max-w-2xl lg:flex-nowrap lg:mb-44 lg:max-w-7xl xl:mb-20 xl:min-h-120  2xl:max-w-[1800px] 2xl:min-h-150 2xl:content-center 2xl:mb-40'>
+        <div className='hidden relative w-full order-2 md:block  md:max-w-4xl md:min-w-96 lg:mb-24 lg:order-1 lg:max-w-xl:min-h-130 lg:max-w-lg lg:mb-0 2xl:max-w-4xl'>
           <Image
             src='/images/img-pages/hero-img.png'
             alt='hero of the page'
@@ -37,7 +37,7 @@ export default function Home() {
             priority={false}
           />
         </div>
-        <div className='h-auto mb-9 sm:max-w-2xl md:w-2/2 md:max-w-3xl lg:max-w-lg 2xl:max-w-2xl'>
+        <div className='h-auto mb-9 sm:max-w-2xl md:w-2/2 md:max-w-3xl lg:mb-0 lg:max-w-lg 2xl:max-w-2xl'>
           <h1 className='mb-10 text-4xl md:text-5xl md:mb-14 2xl::text-6xl font-bold text-primaryBlue'>
             Easy Gift
           </h1>
@@ -54,7 +54,7 @@ export default function Home() {
           </Button>
         </div>
       </section>
-      <section className='mb-28 mx-auto min-h-175 h-full bg-slate-200 flex flex-col justify-center items-center sm:min-h-160 md:min-h-170 lg:flex-wrap lg:flex-row lg:mb-32 lg:content-center lg:justify-around lg:min-h-140 2xl:pb-28 2xl:items-start 2xl:justify-center 2xl:mb-20 2xl:content-end 2xl:min-h-150 2xl:flex-row'>
+      <section className='mb-28 mx-auto min-h-175 h-full bg-slate-200 flex flex-col justify-center items-center sm:min-h-160 md:min-h-170 lg:pt-20 lg:pb-16 lg:flex-wrap lg:flex-row lg:mb-32 lg:content-center lg:justify-around lg:min-h-140 2xl:pt-0 xl:pb-0 2xl:items-start 2xl:justify-center 2xl:mb-20 2xl:content-end 2xl:min-h-150 2xl:flex-row'>
         <h2 className='w-4/5 text-3xl mb-8 text-primaryRed sm:text-center md:mb-10 md:text-4xl lg:w-full lg:mb-14 font-bold 2xl:text-5xl 2xl:mt-0 2xl:mb-28'>
           Crée ton groupe
         </h2>
@@ -83,7 +83,7 @@ export default function Home() {
             Essaie gratuitement
           </Button>
         </article>
-        <Carousel className='max-w-96 max-h-140 sm:max-w-xl 2xl:h-[576px] lg:order-2'>
+        <Carousel className='max-w-96 max-h-140 sm:max-w-xl lg:mb-28 lg:ml-16 lg:mr-16 2xl:h-[576px] lg:order-2'>
           <CarouselPrevious className='top-[-25px] left-8 md:top-2/4'>
             Précédent
           </CarouselPrevious>
