4051ff1159300e91341d6c313b8a0a14f225933f
Jeremie Pourageaux <jpourageaux@hotmail.fr>
2024-06-12 09:42:45 +0200

    rollback

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -45,6 +45,7 @@ schema.then(async schema => {
             origin: [
                 'http://localhost:3000',
                 'https://studio.apollographql.com',
+                'https://staging.0923-bleu-3.wns.wilders.dev/',
             ],
             credentials: true,
         }),
