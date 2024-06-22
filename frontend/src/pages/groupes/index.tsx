0f2f64a3e671d01c74604dba44cbd6b1924a28e1
Jérémie POURAGEAUX <jp@fondation-jae.org>
2024-06-22 21:52:47 +0200

    Add a add group button

--- frontend/src/pages/groupes/index.tsx
+++ frontend/src/pages/groupes/index.tsx
@@ -12,11 +12,10 @@ export default function Profile() {
 
   return (
     <>
-      <section className="mb-10 h-auto flex-col flex-initial flex-wrap justify-evenly items-center mx-auto w-10/12  2xl:mb-40 md:max-w-2xl lg:max-w-4xl xl:max-w-[1100px]">
-        <h2 className="mb-3 text-xl md:mb-3 lg:text-2xl 2xl:text-3xl font-bold text-primaryBlue">
+      <section className="flex flex-col gap-6 pb-6 justify-between mx-auto w-10/12 md:max-w-2xl lg:max-w-4xl xl:max-w-[1100px]">
+        <h2 className="text-xl lg:text-2xl 2xl:text-3xl font-bold text-primaryBlue">
           Mes groupes
         </h2>
-
         {data?.userGroups?.length === 0 ? (
           <div className="flex flex-col items-center mt-6">
             <p className="text-md  mb-3 md:mb-4 lg:mb-6 xl:mb-8 2xl:mb-10 2xl:text-xl">
@@ -30,9 +29,16 @@ export default function Profile() {
           </div>
         ) : (
           <>
-            <p className="text-md text-left mb-3 md:mb-4 lg:mb-6 xl:mb-8 2xl:mb-10 2xl:text-xl">
-              Accède à tes groupes Easy Gift.
-            </p>
+            <div className={'flex justify-between items-center'}>
+              <p className="text-md 2xl:text-xl">
+                Accède à tes groupes Easy Gift.
+              </p>
+              <Button>
+                <a type="button" href="/creating-groups">
+                  Ajouter un groupe
+                </a>
+              </Button>
+            </div>
             <div className="flex flex-wrap gap-6 justify-center">
               {data?.userGroups?.map((group) => (
                 <GroupCard key={group.id} group={group} />
