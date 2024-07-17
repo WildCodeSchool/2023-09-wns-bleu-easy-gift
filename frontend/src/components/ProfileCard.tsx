aefe092661321aa2c67b5b508043170ffd6d34e5
caudrel <lozachaurelie@gmail.com>
2024-07-17 11:20:19 +0200

    fix types issues

--- frontend/src/components/ProfileCard.tsx
+++ frontend/src/components/ProfileCard.tsx
@@ -1,37 +1,40 @@
-import {UserToGroup} from "@/graphql/generated/schema";
-import { Badge } from "@/components/ui/badge"
-
+import { UserToGroup } from "@/graphql/generated/schema";
+import { Badge } from "@/components/ui/badge";
 
 interface UserComponentProps {
-    user: UserToGroup;
+  userToGroup: {
+    is_admin: boolean;
+    user_id: number;
+    group_id: number;
+    user: {
+      email: string;
+      pseudo: string;
+      avatar?: {
+        id: number;
+        url: string;
+      } | null;
+    };
+  };
 }
-export default function ProfileCard({user}: UserComponentProps) {
-    return (
-        <div
-            className="flex justify-between w-full border rounded-2xl transition ease-in-out hover:-translate-y-1 hover:scale-120 duration-300">
-            <div className="flex m-3 lg:min-w-72 items-center gap-3 p-2 ">
-                <img
-                    src={user.user.avatar?.url}
-                    className="w-10 h-10 rounded-full border-solid border-2 border-primaryRed"
-                    alt="Avatar of the user"
-                    title={user.user.pseudo}
-                />
-                <div>
-                    <div className="flex justify-between">
-                        <div className="font-bold">
-                            {user.user.pseudo}
-                        </div>
-                    </div>
-                    <div className="text-sm text-gray-500">
-                        {user.user.email}
-                    </div>
-                </div>
-            </div>
-            <div className="mt-2 mr-3">
-            {(user.is_admin) && (
-                <Badge variant="outline">Admin</Badge>
-                )}
-            </div>
+
+export default function ProfileCard({ userToGroup }: UserComponentProps) {
+  return (
+    <div className="flex justify-between w-full border rounded-2xl transition ease-in-out hover:-translate-y-1 hover:scale-120 duration-300">
+      <div className="flex m-3 lg:min-w-72 items-center gap-3 p-2 ">
+        <img
+          src={userToGroup.user.avatar?.url}
+          className="w-10 h-10 rounded-full border-solid border-2 border-primaryRed"
+          alt="Avatar of the user"
+          title={userToGroup.user?.pseudo}
+        />
+        <div>
+          <div className="flex justify-between">
+            <div className="font-bold">{userToGroup.user.pseudo}</div>
+          </div>
+          <div className="text-sm text-gray-500">{userToGroup.user.email}</div>
         </div>
-    );
+      </div>
+      <div className="mt-2 mr-3">{userToGroup.is_admin && <Badge variant="outline">Admin</Badge>}</div>
+    </div>
+  );
 }
