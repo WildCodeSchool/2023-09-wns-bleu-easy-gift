279f2c425bcfb554eeb7cda0c73b9abd047776da
Jeremie Pourageaux <jpourageaux@hotmail.fr>
2024-07-17 16:14:06 +0200

    fix typage

--- frontend/src/pages/group/[groupId].tsx
+++ frontend/src/pages/group/[groupId].tsx
@@ -12,7 +12,9 @@ export default function GroupDetails() {
   const router = useRouter();
   const { groupId } = router.query;
   const { data, loading, error } = useGetGroupByIdQuery({
-    variables: { groupId: typeof groupId === "string" ? parseInt(groupId, 10) : 0 },
+    variables: {
+      groupId: typeof groupId === "string" ? parseInt(groupId, 10) : 0,
+    },
     fetchPolicy: "no-cache",
     skip: typeof groupId === "undefined",
   });
@@ -28,8 +30,12 @@ export default function GroupDetails() {
     <div>
       <div className="flex flex-col justify-between align-center text-left mb-5">
         <div className="flex flex-col gap-3 justify-between mx-auto w-10/12 md:max-w-2xl lg:max-w-4xl xl:max-w-[1100px]">
-          <h2 className="text-xl lg:text-2xl 2xl:text-3xl font-bold text-primaryBlue">Groupe "{group?.name}"</h2>
-          <p className="text-md 2xl:text-xl">Gère les informations de ton groupe Easy Gift.</p>
+          <h2 className="text-xl lg:text-2xl 2xl:text-3xl font-bold text-primaryBlue">
+            Groupe "{group?.name}"
+          </h2>
+          <p className="text-md 2xl:text-xl">
+            Gère les informations de ton groupe Easy Gift.
+          </p>
           <div className="bg-white flex flex-col gap-10 p-8 sm:rounded-xl shadow-2xl">
             <div className="flex flex-col gap-8 items-center sm:items-start sm:flex-row sm:gap-16 sm:justify-between">
               <div className="shrink sm:w-1/2 sm:max-w-lg">
@@ -38,7 +44,10 @@ export default function GroupDetails() {
                 </div>
               </div>
               <div>
-                <div className="relative w-24 h-24 lg:w-28 lg:h-28 2xl:w-32 2xl:h-32" onClick={() => setIsModalAvatarOpen(!isModalAvatarOpen)}>
+                <div
+                  className="relative w-24 h-24 lg:w-28 lg:h-28 2xl:w-32 2xl:h-32"
+                  onClick={() => setIsModalAvatarOpen(!isModalAvatarOpen)}
+                >
                   <img
                     src={group?.avatar?.url}
                     className="absolute inset-0 w-24 h-24 lg:w-28 lg:h-28 2xl:w-32 2xl:h-32 rounded-full mr-2 border-solid border-4 border-primaryRed"
@@ -56,8 +65,12 @@ export default function GroupDetails() {
             <div className="flex flex-col gap-8 items-center sm:items-start sm:flex-row sm:gap-16 sm:justify-between">
               <div className="shrink sm:w-1/2 sm:max-w-lg">
                 <div>
-                  <div className="text-2xl font-medium">Informations du groupe</div>
-                  <div className="text-sm text-black/60">Voici les informations de votre groupe</div>
+                  <div className="text-2xl font-medium">
+                    Informations du groupe
+                  </div>
+                  <div className="text-sm text-black/60">
+                    Voici les informations de votre groupe
+                  </div>
                 </div>
               </div>
               <div className="shrink sm:w-1/2 sm:max-w-lg">
@@ -73,17 +86,25 @@ export default function GroupDetails() {
                   </div>
                   <Separator />
                   <div className="flex items-center h-9 md:h-11 lg:h-12 2xl:h-14">
-                    <p className="text-base font-semibold w-44">Date de l'évenement</p>
+                    <p className="text-base font-semibold w-44">
+                      Date de l'évenement
+                    </p>
                     <p className="text-base">{group?.event_date}</p>
                   </div>
                   <div className="flex sm:justify-end">
-                    <Button className="bg-blue-500 text-white px-4 py-2 rounded mt-10" onClick={() => setShowModal(true)}>
+                    <Button
+                      className="bg-blue-500 text-white px-4 py-2 rounded mt-10"
+                      onClick={() => setShowModal(true)}
+                    >
                       Modifier mes informations
                     </Button>
                     {showModal &&
                       createPortal(
                         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-                          <ModalUpdateGroup onClose={() => setShowModal(false)} group={group} />
+                          <ModalUpdateGroup
+                            onClose={() => setShowModal(false)}
+                            group={group}
+                          />
                         </div>,
                         document.body
                       )}
@@ -98,12 +119,19 @@ export default function GroupDetails() {
               <div className="shrink sm:w-1/2 sm:max-w-lg">
                 <div>
                   <div className="text-2xl font-medium">Membres du groups</div>
-                  <div className="text-sm text-black/60">Voici la liste des membres de votre groupe</div>
+                  <div className="text-sm text-black/60">
+                    Voici la liste des membres de votre groupe
+                  </div>
                 </div>
               </div>
               <div className="flex flex-col gap-3 shrink items-center sm:w-1/2 sm:max-w-lg">
                 {group?.userToGroups.map((userToGroup) => {
-                  return <ProfileCard key={userToGroup.user_id} userToGroup={userToGroup} />;
+                  return (
+                    <ProfileCard
+                      key={userToGroup.user_id}
+                      userToGroup={userToGroup}
+                    />
+                  );
                 })}
               </div>
             </div>
