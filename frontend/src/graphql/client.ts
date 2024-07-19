b7d0d03ede3c7a290373709333a2323671cd6114
dlopoelinreverse <l.lesaul@gmail.com>
2024-07-19 14:33:38 +0200

    fix 2

--- frontend/src/graphql/client.ts
+++ frontend/src/graphql/client.ts
@@ -1,48 +1,3 @@
-// import { ApolloClient, HttpLink, split, InMemoryCache } from '@apollo/client';
-// import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
-// import { getMainDefinition } from '@apollo/client/utilities';
-// import { createClient } from 'graphql-ws';
-// require('dotenv').config();
-
-// const devApiUrl = process.env.NEXT_PUBLIC_APOLLO_URI
-
-
-// const wsUrl = 'ws://localhost:4001';
-// const wsLink = new GraphQLWsLink(
-//   createClient({
-//     url: wsUrl!,
-//   })
-// );
-
-// const httpLink = new HttpLink({
-//   uri: devApiUrl ? devApiUrl : '/graphql',
-//   credentials: 'include',
-// });
-
-
-// const splitLink = split(
-//   ({ query }) => {
-//     const definition = getMainDefinition(query);
-//     return (
-//       definition.kind === 'OperationDefinition' &&
-//       definition.operation === 'subscription'
-//     );
-//   },
-//   wsLink,
-//   httpLink
-// );
-
-// const client = new ApolloClient({
-//   link: splitLink,
-//   cache: new InMemoryCache(),
-//   defaultOptions: {
-//     watchQuery: {
-//       nextFetchPolicy: 'cache-and-network',
-//     },
-//   },
-// });
-
-// export default client
 import { ApolloClient, HttpLink, split, InMemoryCache } from '@apollo/client';
 import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
 import { getMainDefinition } from '@apollo/client/utilities';
@@ -51,31 +6,16 @@ require('dotenv').config();
 
 const devApiUrl = process.env.NEXT_PUBLIC_APOLLO_URI
 
-console.log("_______ENV", process.env.NEXT_PUBLIC_APOLLO_URI)
-
-// const wsUrl = devApiUrl
-//   ? devApiUrl.replace(/^http/, 'ws') + '/subscriptions'
-//   : '/subscriptions';
-// const wsUrl = process.env.NEXT_PUBLIC_APOLLO_WB_URI;
 const wsUrl = 'ws://localhost:4001';
 const wsLink = new GraphQLWsLink(
   createClient({
     url: wsUrl!,
   })
 );
-// const httpLink = new HttpLink({
-//   uri: devApiUrl ? devApiUrl : '/graphql',
-//   credentials: 'include',
-// });
 const httpLink = new HttpLink({
-//   uri: 'http://localhost:4001/graphql',
   uri: devApiUrl ? devApiUrl : '/graphql',
   credentials: 'include',
 });
-console.log('wsLink', wsLink);
-console.log('httpLink', httpLink);
-console.log('WebSocket URL:', wsUrl);
-console.log('devApiUrl', devApiUrl);
 
 const splitLink = split(
   ({ query }) => {
@@ -88,7 +28,6 @@ const splitLink = split(
   wsLink,
   httpLink
 );
-console.log('splitLink', splitLink);
 
 const client = new ApolloClient({
   link: splitLink,
