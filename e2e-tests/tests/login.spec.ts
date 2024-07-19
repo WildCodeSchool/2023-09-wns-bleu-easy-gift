0242af047aa7e4e060a4e2678a1fdb2a3d9de492
dlopoelinreverse <l.lesaul@gmail.com>
2024-07-19 10:30:27 +0200

    add timeout 10000 on page after login

--- e2e-tests/tests/login.spec.ts
+++ e2e-tests/tests/login.spec.ts
@@ -26,7 +26,7 @@ test("has a valid user name", async ({ page }) => {
   await page.getByTestId("login-email").fill(emailToCheck);
   await page.getByTestId("login-password").fill(passwordToCheck);
   await page.getByRole("button", { name: "Se connecter" }).click();
-  await page.waitForTimeout(5000);
+  await page.waitForTimeout(10000);
   const disconnectButton = page.getByRole("button", {
     name: "Se déconnecter",
   });
