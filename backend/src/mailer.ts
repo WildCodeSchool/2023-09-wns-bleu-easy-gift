2b429322d98307dca144f5a14d6bcce23c35e900
Jeremie Pourageaux <jpourageaux@hotmail.fr>
2024-07-25 17:44:59 +0200

    fix mail on vps

--- backend/src/mailer.ts
+++ backend/src/mailer.ts
@@ -1,3 +1,5 @@
+//-------------------SMTP
+
 // import nodemailer from 'nodemailer'
 // import * as dotenv from 'dotenv'
 // dotenv.config()
@@ -28,6 +30,8 @@
 //               },
 //           }
 // }
+
+//--------------------HTTP
 
 import * as dotenv from 'dotenv'
 dotenv.config()
